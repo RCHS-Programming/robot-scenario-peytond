5a0175e8c90b355bc3e6aca30bdb0f2d434cffb1
peytond <peyton.dunn@stu.boone.kyschools.us>
2021-03-29 08:39:19 -0400

    added all characters, started making robot move

---
+++ Robot.java
@@ -0,0 +1,34 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Robot here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Robot extends Actor
+{
+    /**
+     * Act - do whatever the Robot wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        robotMovement();
+    }  
+    public void robotMovement()
+    {
+        if (Greenfoot.isKeyDown("up"))
+        {
+            setLocation( getX(), getY() -3 );
+        }
+        if(Greenfoot.isKeyDown("down"))
+        {
+            setLocation( getX(), getY() +3 );
+        }
+        if(Greenfoot.isKeyDown("left"))
+        {
+            
+        }
+    }
+}
---
+++ RobotWorld.java
@@ -0,0 +1,37 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class RobotWorld here.
+ * 
+ * @author Peyton Dunn
+ * @version 3/29/21
+ */
+public class RobotWorld extends World
+{
+
+    /**
+     * Constructor for objects of class RobotWorld.
+     * 
+     */
+    public RobotWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(800, 600, 1); 
+        
+        addObject (new Wall(), 51, 140); 
+        addObject (new Wall(), 158, 140);
+        addObject (new Wall(), 264, 140);
+        addObject (new Block(), 418, 130);
+        addObject (new Wall(), 573, 140);
+        addObject (new Wall(), 680, 140);
+        addObject (new Wall(), 787, 140);
+        addObject (new Home(), 740, 533);
+        addObject (new Scorepanel(), 89, 536);
+        
+        addObject (new Pizza(), 737, 42);
+        addObject (new Pizza(), 426, 42);
+        addObject (new Pizza(), 430, 243);
+        addObject (new Pizza(), 716, 369);
+        addObject (new Pizza(), 155, 369);
+    }
+}
